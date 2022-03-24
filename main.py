4d143a4fa80eed09ee002b0593e3db9e6c56dd24
orplazh <vshuliar01@gmail.com>
2022-03-24 22:34:57 +0200

    2022_03_24_22_34

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 """Bot."""
 
 
+from os import PRIO_PGRP
+
+
 class Team:
     coeffs = {
         "Form": 0.25,
@@ -12,6 +15,7 @@ class Team:
         "Motivation": 0.05,
         "Mood": 0.05,
     }
+    score = 0
 
     def __init__(self, name):
         self.name = name
@@ -43,25 +47,53 @@ class Team:
     def set_mood(self, mood):
         self.mood = mood
 
-    def chance(self, coeffs):
-        self.chance = (
-            self.form * coeffs["Form"]
-            + self.head_to_head * coeffs["Head-to-head"]
-            + self.absent_players * coeffs["Absent players"]
-            + self.home_away * coeffs["Home/away"]
-            + self.table * coeffs["Table"]
-            + self.standings * coeffs["Standings"]
-            + self.rest * coeffs["Rest"]
-            + self.motivation * coeffs["Motivation"]
-            + self.mood * coeffs["Motivation"]
-        )
-
-    def predict(self, opponent):
-        pass
-
     def change_coeffs(self, new_coeffs):
         self.coeffs = new_coeffs
 
+    def predict(self, opponent):
+        self.__zip()
+        opponent.__zip()
+
+        for _, coeff in enumerate(list(self.coeffs.values())):
+            a, b = Team.__chance(self.data[_], opponent.data[_], coeff)
+            self.score += a
+            opponent.score += b
+
+        return self.score, opponent.score
+
+    def __chance(a, b, coeff):
+        if a == b:
+            return a, b
+        if a < b:
+            b /= float(f"1.{int(100 / (a + b) * (b - a) * 0.8)}")
+
+        else:
+            a /= float(f"1.{int(100 / (a + b) * (a - b) * 0.8)}")
+        return round(100 / (a + b) * a * coeff, 1), round(100 / (a + b) * b * coeff, 1)
+
+    def __zip(self):
+        self.data = list(self.__dict__.values())[1:]
+
 
 if __name__ == "__main__":
-    pass
+    team_a = Team("Barcelona")
+    team_a.set_form(0)
+    team_a.set_head_to_head(0)
+    team_a.set_absent_players(0)
+    team_a.set_home_away(0)
+    team_a.set_standings(0)
+    team_a.set_rest(0)
+    team_a.set_motivation(0)
+    team_a.set_mood(0)
+
+    team_b = Team("Lazio")
+    team_b.set_form(1)
+    team_b.set_head_to_head(1)
+    team_b.set_absent_players(1)
+    team_b.set_home_away(1)
+    team_b.set_standings(1)
+    team_b.set_rest(1)
+    team_b.set_motivation(1)
+    team_b.set_mood(1)
+
+    print(team_a.predict(team_b))
