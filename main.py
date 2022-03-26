22006fd160a0b0b9908a340f1bb22f1822a68334
orplazh <vshuliar01@gmail.com>
2022-03-26 16:21:37 +0200

    2022_03_26_16_21

--- main.py
+++ main.py
@@ -101,8 +101,8 @@ class Team:
     def set_motivation(self) -> None:
         """The team is motivated to win if it is in first or last 35% of the table."""
         if self.place in (
-            list(range(1, int(self.standings * 0.35)))
-            + list(range(int(self.standings * 0.65), self.standings))
+            list(range(1, int(self.standings * 0.35) + 1))
+            + list(range(int(self.standings * 0.65), self.standings + 1))
         ):
             self.motivation = 1
         else:
@@ -128,23 +128,88 @@ class Team:
 
     def predict(self, opponent: "Team") -> str:
         """Predicts the possible winner of the upcoming match."""
+        comparison = []
+        comparison.append(
+            Team.__update_with_coeff(
+                self.__compare_form(self.form, opponent.form),
+                self.coeffs["form"],
+            )
+        )
+        comparison.append(
+            Team.__update_with_coeff(
+                self.__compare_head_to_head(self.head_to_head, opponent.head_to_head),
+                self.coeffs["head_to_head"],
+            )
+        )
+        comparison.append(
+            Team.__update_with_coeff(
+                self.__compare_absent_players(
+                    self.absent_players, opponent.absent_players
+                ),
+                self.coeffs["absent_players"],
+            )
+        )
+        comparison.append(
+            Team.__update_with_coeff(
+                self.__compare_home_away(self.home_away, opponent.home_away),
+                self.coeffs["home_away"],
+            )
+        )
+        comparison.append(
+            Team.__update_with_coeff(
+                self.__compare_place(self.place, opponent.place),
+                self.coeffs["place"],
+            )
+        )
+        comparison.append(
+            Team.__update_with_coeff(
+                self.__compare_rest(self.rest, opponent.rest),
+                self.coeffs["rest"],
+            )
+        )
+        comparison.append(
+            Team.__update_with_coeff(
+                self.__compare_motivation(self.motivation, opponent.motivation),
+                self.coeffs["motivation"],
+            )
+        )
+        comparison.append(
+            Team.__update_with_coeff(
+                self.__compare_mood(self.mood, opponent.mood),
+                self.coeffs["mood"],
+            )
+        )
+        for _ in comparison:
+            self.score += _[0]
+        opponent.score = 100 - self.score
+        print(f"Form: ({self.form}, {opponent.form})")
         print(self.__compare_form(self.form, opponent.form))
+        print(f"Head-to-head: ({self.head_to_head}, {opponent.head_to_head})")
         print(self.__compare_head_to_head(self.head_to_head, opponent.head_to_head))
+        print(f"Absent players: ({self.absent_players}, {opponent.absent_players})")
         print(
             self.__compare_absent_players(self.absent_players, opponent.absent_players)
         )
+        print(f"Home/away: ({self.home_away}, {opponent.home_away})")
         print(self.__compare_home_away(self.home_away, opponent.home_away))
+        print(f"Place: ({self.place}, {opponent.place})")
         print(self.__compare_place(self.place, opponent.place))
+        print(f"Rest: ({self.rest}, {opponent.rest})")
         print(self.__compare_rest(self.rest, opponent.rest))
+        print(f"Motivation: ({self.motivation}, {opponent.motivation})")
         print(self.__compare_motivation(self.motivation, opponent.motivation))
+        print(f"Mood: ({self.mood}, {opponent.mood})")
         print(self.__compare_mood(self.mood, opponent.mood))
+        print("\nRESULT WITH COEFFS")
+        return f"<{self.name} {self.score}%> VS <{opponent.score}% {opponent.name}>"
 
     def __evaluate_with_maximum(a, b, max_num):
         """Evaluates the percentage."""
         if a == b:
             return 50, 50
         gap = max([a, b]) - min([a, b])
-        k = 100 / (a + b) / (max_num / gap) * gap / 2
+        k = (50 * gap**2) / ((a + b) * max_num)
+
         if a < b:
             a = round(50 - k, 1)
             b = round(100 - a, 1)
@@ -158,7 +223,7 @@ class Team:
         if a == b:
             return 50, 50
         gap = max([a, b]) - min([a, b])
-        k = 100 / (a + b) / (min_num / gap) * gap / 2
+        k = (50 * gap**2) / ((a + b) * min_num)
         if a < b:
             a = round(50 + k, 1)
             b = round(100 - a, 1)
@@ -167,6 +232,10 @@ class Team:
             a = round(100 - b, 1)
         return a, b
 
+    def __update_with_coeff(pair: tuple, coeff: float) -> tuple:
+        """Updates percentage with coefficient."""
+        return round(pair[0] * coeff, 1), round(pair[1] * coeff, 1)
+
     def __input_check(var, scale):
         """Checks whether number is less than one."""
         if var >= scale:
@@ -174,31 +243,93 @@ class Team:
         return var
 
 
-if __name__ == "__main__":
-    team_a = Team("Barcelona")
-    team_a.set_form(6)
-    team_a.set_head_to_head(1)
-    team_a.set_absent_players(4)
-    team_a.set_standings(20)
-    team_a.set_place(3)
-    team_a.set_home_away(5)
-    team_a.set_rest(6)
+def main():
+    """The main fucntion."""
+    data = receive_input()
+    team_a = Team(data["name"])
+    team_a.set_form(data["form"])
+    team_a.set_head_to_head(data["head_to_head"])
+    team_a.set_absent_players(data["absent_players"])
+    team_a.set_standings(data["standings"])
+    team_a.set_place(data["place"])
+    team_a.set_home_away(data["home_away"])
+    team_a.set_rest(data["home_away"])
     team_a.set_motivation()
-    team_a.set_mood(0)
+    team_a.set_mood(data["mood"])
 
-    team_b = Team("Lazio")
-    team_b.set_form(7)
-    team_b.set_head_to_head(2)
-    team_b.set_absent_players(3)
-    team_b.set_standings(20)
-    team_b.set_place(10)
-    team_b.set_home_away(10)
-    team_b.set_rest(9)
+    data = receive_input()
+    team_b = Team(data["name"])
+    team_b.set_form(data["form"])
+    team_b.set_head_to_head(data["head_to_head"])
+    team_b.set_absent_players(data["absent_players"])
+    team_b.set_standings(data["standings"])
+    team_b.set_place(data["place"])
+    team_b.set_home_away(data["home_away"])
+    team_b.set_rest(data["home_away"])
     team_b.set_motivation()
-    team_b.set_mood(1)
+    team_b.set_mood(data["mood"])
 
     print(team_a.predict(team_b))
 
-    # for _ in range(6):
-    #     for i in range(6):
-    #         print((_, i), "=", team_a.compare_absent_players(_, i))
+
+def receive_input():
+    """Receives user's input."""
+    data = {}
+    print("Team name:")
+    data["name"] = input(">>> ")
+    print("Form:")
+    data["form"] = check_int()
+    print("Head-to-head:")
+    data["head_to_head"] = check_int()
+    print("Number of absent players:")
+    data["absent_players"] = check_int()
+    print("Standings size:")
+    data["standings"] = check_int()
+    print("Place in the standings:")
+    data["place"] = check_int()
+    print("Home/away:")
+    data["home_away"] = check_int()
+    print("Rest:")
+    data["rest"] = check_int()
+    print("Mood:")
+    data["mood"] = check_int()
+
+    print("\nSuccess!\n")
+    return data
+
+
+def check_int():
+    """Checks whether input is an integer number."""
+    try:
+        return int(input(">>> "))
+    except ValueError:
+        print("Error. Try again.")
+        return check_int()
+
+
+if __name__ == "__main__":
+    main()
+
+    # team_a = Team("Barcelona")
+    # team_a.set_form(6)
+    # team_a.set_head_to_head(1)
+    # team_a.set_absent_players(4)
+    # team_a.set_standings(20)
+    # team_a.set_place(3)
+    # team_a.set_home_away(5)
+    # team_a.set_rest(6)
+    # team_a.set_motivation()
+    # team_a.set_mood(0)
+
+    # team_b = Team("Lazio")
+    # team_b.set_form(7)
+    # team_b.set_head_to_head(2)
+    # team_b.set_absent_players(3)
+    # team_b.set_standings(20)
+    # team_b.set_place(20)
+    # team_b.set_home_away(10)
+    # team_b.set_rest(9)
+    # team_b.set_motivation()
+    # team_b.set_mood(1)
+
+    # print(team_a.predict(team_b))
